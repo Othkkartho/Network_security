989128c68b870dcfa8bdc5da7d3ecd3d3b014cac
Othkkartho <kimwonkyu15@gmail.com>
2022-12-05 10:40:25 +0900

    다이제스트 메세지 생성에 비밀키 활용

--- src/MsgDigest.java
+++ src/MsgDigest.java
@@ -1,20 +1,27 @@
-import java.security.MessageDigest;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import java.security.Key;
+import java.security.SecureRandom;
 
 public class MsgDigest {
     public static void main(String[] args) {
         try {
             // 인스턴스 생성
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            String msg = "Welcome to Java JCA!!";
+            KeyGenerator generator = KeyGenerator.getInstance("DES"); // 대칭키 방식, 비밀키 생성기를 만듦
+            SecureRandom random = new SecureRandom();
+            generator.init(random); // random을 통해 generator를 초기화
+            Key key = generator.generateKey();  // 비밀키 생성
 
-            digest.update(msg.getBytes());
-            byte[] bytes = digest.digest();    // digest 생성
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(key);  // 다이제스트 메세지 생성에 비밀키를 활용
 
-            StringBuilder builder = new StringBuilder();
+            String msg = "Welcom to java JCA!!";
+            byte[] bytes = mac.doFinal(msg.getBytes());
+
+            StringBuilder sb = new StringBuilder();
             for (byte b : bytes)
-                builder.append(String.format("%02x", b));
-
-            System.out.println(builder);
+                sb.append(String.format());
+            System.out.println(sb.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
